8ae2b9e858ec296a6605fe4dc2178b85bf74add6
razzyX7 <Razzysolo442@gmail.com>
2025-06-01 18:41:24 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -450,9 +450,8 @@ async def on_shutdown(dispatcher: Dispatcher):
     await dispatcher.storage.wait_closed()
     
 if __name__ == '__main__':
-    try:
-        executor.start_polling(dp, skip_updates=True, on_shutdown=on_shutdown, loop=loop)
-    except Exception as e:
-        print(f"Ошибка запуска бота: {e}")
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
+
 
     
